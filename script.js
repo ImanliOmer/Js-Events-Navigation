43463c066dab118152dbf1d4fd6163560a914ab6
omer imanov <omer.imanov@gmail.com>
2022-05-25 17:10:14 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//Task1
+
+// swal("Good job!", "You clicked the button!", "success");
+
+// let input = document.getElementById("input")
+// input.addEventListener("focus", function(){
+//     console.log("focused");
+//     this.setAttribute("class", "def")
+// })
+
+// input.addEventListener("blur", function(){
+//     console.log("blurred"); 
+//     swal("You wrote: " + input.value);
+// })
+
+// input.addEventListener("keyup", function(){
+//     console.log(input.value);
+// })
+
+
+// let cars = document.getElementById("cars")
+// cars.addEventListener("change", function(){
+//     console.log(cars.value);
+// })
+// console.log(cars.value);
+
+// let form = document.getElementById("form")
+// let fname = document.getElementById("fname")
+// let lname = document.getElementById("lname")
+// document.onkeyup=function(e){
+//     console.log(e.keyCode);
+// }
